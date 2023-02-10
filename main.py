1732cc3eece032ba96d6df597f0b2d24f96c9efd
Natan J Mai <natan.mai@hotmail.com>
2023-02-10 14:17:47 +1030

    Menu basic control.

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def pg_home():
     return 2
 
 def main():
-    choose = int(input('''------- CHOOSE ------\n1. START DATABASES\n2. TEST INSERT RECORDS\n3. SHOW RECORDS'''))
-
-    if choose == 1:
-        database.start_database()
-    elif choose == 2:
-        tests.test_insert()
-    elif choose == 3:
-        tests.show_all()
+    while(1):
+        try:
+            choose = int(input('''------- CHOOSE ------\n1. START DATABASES\n2. TEST INSERT RECORDS\n3. SHOW RECORDS'''))
+        except:
+            print("Please type a correct option.")
+        else:
+            if choose == 1:
+                database.start_database()
+            elif choose == 2:
+                tests.test_insert()
+            elif choose == 3:
+                tests.show_all()
+            else:
+                return
 
 if __name__ == "__main__":
     main()
