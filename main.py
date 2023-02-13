d1aa9a8633751f3198fcc3fc6f5b4f2b9e98f8b4
Natan J Mai <natan.mai@hotmail.com>
2023-02-14 09:27:27 +1030

    Testing PyWebView

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import eel
 import database.db_functions as database
 import tests
+import webview
 
-from random import randint
+def view_page():
+    with open('web/index.html', 'r') as file:
+        html = file.read().replace('\n', '')
+    window = webview.create_window('Testing PyWebView', html=html, width=800, height=600)
+    webview.start()
 
-@eel.expose
-def pg_home():
-    return 2
+    return
 
 def main():
     while(1):
         try:
-            choose = int(input('''------- CHOOSE ------\n1. START DATABASES\n2. TEST INSERT RECORDS\n3. SHOW RECORDS'''))
+            choose = int(input('''------- CHOOSE ------\n0. SHOW PAGE\n1. START DATABASES\n2. TEST INSERT RECORDS\n3. SHOW RECORDS'''))
         except:
             print("Please type a correct option.")
         else:
-            if choose == 1:
+            if choose == 0:
+                view_page()
+            elif choose == 1:
                 database.start_database()
             elif choose == 2:
                 tests.test_insert()
